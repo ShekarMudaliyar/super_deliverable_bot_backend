209b5d1180f5389a7fb7a4fcc0ae238c7cd5b02d
Shekar Mudaliyar <b1nary@Shekars-MacBook-Pro.local>
2019-10-21 15:14:11 +0530

    routes and webhooks

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { WebClient } = require("@slack/web-api");
+const { IncomingWebhook } = require("@slack/webhook");
 
 const app = express();
 dotenv.config();
@@ -16,6 +18,20 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 4500;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  // Read a url from the environment variables
+  const url = process.env.SLACK_WEBHOOK_URL;
+
+  // Initialize
+  const webhook = new IncomingWebhook(url);
+  // Send the notification
+  (async () => {
+    await webhook.send({
+      text: "> Hello World!!"
+    });
+  })();
+});
+app.post("/sendDaily", (req, res) => {});
+app.post("/sendWeekly", (req, res) => {});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
